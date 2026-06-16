cbddec514016a61389eefe6815a26b0663d97ae5
YuvaBalaji01 <yuvabalajikumar@gmail.com>
2026-06-16 21:35:26 +0530

    practice

---
+++ currencyExchanger/script.js
@@ -0,0 +1,10 @@
+let input = document.querySelector(".ip");
+let bnt = document.querySelector("button");
+
+bnt.addEventListener("click", ()=> {
+    const val = input.value;
+    console.log(val);    
+})
+
+let message = document.querySelector(".msg");
+console.log(message.innerText);
