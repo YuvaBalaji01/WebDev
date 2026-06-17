b513990a668b940ca4a0f83a19d90c19f2c95bb4
YuvaBalaji01 <yuvabalajikumar@gmail.com>
2026-06-17 16:49:59 +0530

    practice

--- currencyExchanger/script.js
+++ currencyExchanger/script.js
@@ -1,3 +1,4 @@
+
 let input = document.querySelector(".ip");
 let bnt = document.querySelector("button");
 
@@ -6,5 +7,26 @@ bnt.addEventListener("click", ()=> {
     console.log(val);    
 })
 
-let message = document.querySelector(".msg");
-console.log(message.innerText);
+// let message = document.querySelector(".msg");
+// console.log(message.innerText);
+
+// let newoption = document.createElement("option");
+// console.dir(newoption);
+
+let tag1 = document.querySelector("#from");
+    let tag2 = document.querySelector("#to");
+    let tags = [tag1,tag2];
+
+for(let tag of tags){
+    for(code in countryList){
+        let newoption = document.createElement("option");
+        newoption.value = code;
+        newoption.innerText = code;
+        tag.appendChild(newoption);  
+        if(code === "USD" && tag.name === "From")
+            newoption.selected = "selected";
+        if(code === "INR" && tag.name === "To")
+            newoption.selected = "selected";
+    }
+}
+
